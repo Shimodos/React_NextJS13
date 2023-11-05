d2efc73ad0924129e9548745ea96193734635e37
Oleksiy Demchenko <ad@pesochin.net>
2023-11-05 22:01:27 +0100

    REACT_NEXTJS: Chapte 3.16 Literal typs

--- test.ts
+++ test.ts
@@ -1,1 +1,33 @@
 // const a: number = 6;
+
+type Point = { x: number; y: number };
+
+type D3Point = Point & { z: number };
+interface IPoint {
+  x: number;
+  y: number;
+}
+
+interface I3DPoint extends IPoint {
+  z: number;
+}
+
+type stringOrNumber = string | number;
+
+const c = (point: IPoint) => {
+  const d: I3DPoint = point as I3DPoint;
+};
+
+const myCanvas = document.getElementById('main_canvas') as HTMLCanvasElement; // Type assertion
+
+function print(coord: Point): void {
+  console.log(x, y);
+}
+
+interface ITest {
+  a: number;
+}
+
+interface ITest {
+  b: number;
+}
