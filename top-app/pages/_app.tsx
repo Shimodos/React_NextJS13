af840c0b64382b7718e9d6f7b30c84719c8c5d88
Alekssey Demchenko <ad@pesochin.net>
2023-11-15 20:19:19 +0200

    REACT_NEXTJS13: Сhapte 7.45 Simple components

--- top-app/pages/_app.tsx
+++ top-app/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
