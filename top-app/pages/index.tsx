528f50cf3c928e029fad587771c1b7d39d7dcaa4
Alekssey Demchenko <ad@pesochin.net>
2023-11-18 01:53:04 +0200

    REACT_NEXTJS13: Сhapte 7.54 components, paragraf, Tag

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -11,6 +11,18 @@ export default function Home(): JSX.Element {
       <Button appearence="ghost" arrow="right">
         Button
       </Button>
+      <P size="l">Larg</P>
+      <P>Medium</P>
+      <P size="s">Small</P>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">
+        Red
+      </Tag>
+      <Tag size="m" color="green">
+        Green
+      </Tag>
+      <Tag color="primary">Primary</Tag>
+      <Tag color="grey">Grey</Tag>
     </>
   );
 }
