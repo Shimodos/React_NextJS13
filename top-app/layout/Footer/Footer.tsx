3674ebd5ff4ce84a734e2943e0c8a668ac7437f4
Alekssey Demchenko <ad@pesochin.net>
2023-11-24 23:54:01 +0200

    REACT_NEXTJS13: Сhapte 10.77 CSS Grid

--- top-app/layout/Footer/Footer.tsx
+++ top-app/layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>AlexDev © 2022 - {format(new Date(), 'yyyy')} All rights reserved</div>
+      <a href="#" target="_blank">
+        Terms of use
+      </a>
+      <a href="#" target="_blank">
+        Privacy Policy
+      </a>
+    </footer>
+  );
 };
